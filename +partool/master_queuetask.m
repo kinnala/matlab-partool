c92e869037bb99ca33a24905244971f03ac26a1c
Tom Gustafsson <tom@valdemar.hut.fi>
2015-05-07 17:46:54 +0300

    Fixed queing

--- +partool/master_queuetask.m
+++ +partool/master_queuetask.m
@@ -4,13 +4,13 @@ function npool=master_queuetask(pool,task,idata)
 % Syntax:
 %   npool=partool.master_queuetask(pool,task,idata)
 %
-if ~isfield(pool,'queue')
-    pool.queue={};
+npool=pool;
+if ~isfield(npool,'queue')
+    npool.queue={};
 end
 
 % add task to queue
 taskstruct=struct;
 taskstruct.task=task;
 taskstruct.idata=idata;
-pool.queue{end+1}=taskstruct;
-npool=pool;
+npool.queue{end+1}=taskstruct;
